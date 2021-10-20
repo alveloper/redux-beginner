48e995d941d23adb6c87a058ceeae3fd9b87a6af
Suyeon <alveloper@gmail.com>
2021-10-20 15:01:11 +0900

    Demystifying your first redux saga

--- src/js/duck.js
+++ src/js/duck.js
@@ -1,26 +1,32 @@
 // Action types
 export const ADD_ARTICLE = "ADD_ARTICLE";
 export const DATA_LOADED = "DATA_LOADED";
+export const DATA_REQUESTED = "DATA_REQUESTED";
+export const API_ERRORED = "API_ERRORED";
 
 // Reducer: which makes the state in Redux
 // Always remember: the state in redux comes from reducers.
 const initialState = {
   articles: [],
-  remoteArticles: []
+  remoteArticles: [],
 };
 
 export default function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
     return Object.assign({}, state, {
-      articles: state.articles.concat(action.payload)
+      articles: state.articles.concat(action.payload),
     });
   }
 
   if (action.type === DATA_LOADED) {
     return Object.assign({}, state, {
-      remoteArticles: state.remoteArticles.concat(action.payload)
+      remoteArticles: state.remoteArticles.concat(action.payload),
     });
   }
+
+  // if (action.type === API_ERRORED) {
+  //   return "Something went wrong."
+  // }
 
   return state;
 }
@@ -29,15 +35,9 @@ export default function rootReducer(state = initialState, action) {
 // Actions: plain JavaScript objects, acting as messengers in your application
 // Action creators: functions for creating those messages.
 export function addArticle(payload) {
-  return { type: ADD_ARTICLE, payload }
-};
+  return { type: ADD_ARTICLE, payload };
+}
 
 export function getData() {
-  return function(dispatch) {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(json => {
-        dispatch({ type: DATA_LOADED, payload: json });
-      });
-  };
+  return { type: DATA_REQUESTED };
 }
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,13 +1,20 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import rootReducer from "../duck";
 import { forbiddenWordsMiddleware } from "../middleware";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
+
+const initializeSagaMiddleware = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+  storeEnhancers(
+    applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleware)
+  )
 );
+
+initializeSagaMiddleware.run(apiSaga);
 
 export default store;
