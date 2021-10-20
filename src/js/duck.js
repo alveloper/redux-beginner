d33d54169d02fce44782f6b6b095232238c44c13
Suyeon <alveloper@gmail.com>
2021-10-20 14:50:24 +0900

    Asynchronous actions in Redux with Redux Thunk

--- src/js/duck.js
+++ src/js/duck.js
@@ -1,10 +1,12 @@
 // Action types
 export const ADD_ARTICLE = "ADD_ARTICLE";
+export const DATA_LOADED = "DATA_LOADED";
 
 // Reducer: which makes the state in Redux
 // Always remember: the state in redux comes from reducers.
 const initialState = {
-  articles: []
+  articles: [],
+  remoteArticles: []
 };
 
 export default function rootReducer(state = initialState, action) {
@@ -13,6 +15,13 @@ export default function rootReducer(state = initialState, action) {
       articles: state.articles.concat(action.payload)
     });
   }
+
+  if (action.type === DATA_LOADED) {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
+    });
+  }
+
   return state;
 }
 
@@ -22,3 +31,13 @@ export default function rootReducer(state = initialState, action) {
 export function addArticle(payload) {
   return { type: ADD_ARTICLE, payload }
 };
+
+export function getData() {
+  return function(dispatch) {
+    return fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(json => {
+        dispatch({ type: DATA_LOADED, payload: json });
+      });
+  };
+}
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,13 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import rootReducer from "../duck";
 import { forbiddenWordsMiddleware } from "../middleware";
+import thunk from "redux-thunk";
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(forbiddenWordsMiddleware)
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
 );
 
 export default store;
